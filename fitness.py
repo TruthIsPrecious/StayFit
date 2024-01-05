ac0ec4b3962656232a5227560e411f1784ac9a3d
TruthIsPrecious <149413755+TruthIsPrecious@users.noreply.github.com>
2024-01-05 11:58:53 -0800

    Create fitness.py
    Listed steps and features for program

---
+++ fitness.py
@@ -0,0 +1,18 @@
+'''
+Fitness program
+
+- Users log into their account with their username AND password to view how many times they exercise in a week (5-day period). 
+- A list is used to store information; it includes an ELEMENT consisting of:
+  * [a username, password, and statistic about the user]
+- The list keeps records of as many users as possible
+- The records will be kept monthly. (This means that previous records are deleted).
+
+ORDER of program
+- A list of profiles
+- User is asked to add an account, delete an account, or log into their account
+  * To add an account: user must provide their username and password
+  * To delete an account: user must provide their password
+  * To modify their username or password: user must provide their password and have the option to enter a new username/password
+- User can quit from the main page at ANY time
+
+'''
